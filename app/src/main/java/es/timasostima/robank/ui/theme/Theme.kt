df1e968fcef211fd8bf439d6ca6d4f957794eb5a
Tymur <tymurkulivar@gmail.com>
2024-12-01 19:20:17 +0100

    updade color and themes

--- app/src/main/java/es/timasostima/robank/ui/theme/Theme.kt
+++ app/src/main/java/es/timasostima/robank/ui/theme/Theme.kt
@@ -9,28 +9,35 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green,
+    background = DarkBlue,
+
+    onSurface = White,
+    onSecondaryContainer = White.copy(alpha = 0.45f),
+    //navbar selection
+    secondaryContainer = White.copy(alpha = 0.15f),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Green,
+    background = White,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSurface = Black,
+    onSecondaryContainer = DarkBlue.copy(alpha = 0.45f),
+    secondaryContainer = DarkBlue.copy(alpha = 0.10f),
+
+//    background = Color(0xFFFFFBFE),
+//    surface = Color(0xFFFFFBFE),
+//    onPrimary = Color.White,
+//    onSecondary = Color.White,
+//    onTertiary = Color.White,
+//    onBackground = Color(0xFF1C1B1F),
+//    onSurface = Color(0xFF1C1B1F),
+
 )
 
 @Composable
@@ -41,10 +48,11 @@ fun RobankTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
+        /*
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        }*/
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
