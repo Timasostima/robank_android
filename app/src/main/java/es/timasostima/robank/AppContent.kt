968447b74a302630fd8af980e31198127e7e71bc
Tymur <tymurkulivar@gmail.com>
2024-12-14 21:50:36 +0100

    Structure updates

---
+++ app/src/main/java/es/timasostima/robank/AppContent.kt
@@ -0,0 +1,164 @@
+package es.timasostima.robank
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import es.timasostima.robank.charts.Charts
+import es.timasostima.robank.config.ConfigScreen
+import es.timasostima.robank.database.BillData
+import es.timasostima.robank.database.CategoryData
+import es.timasostima.robank.database.Database
+import es.timasostima.robank.database.GoalData
+import es.timasostima.robank.database.PreferencesData
+import es.timasostima.robank.enterApp.AccountManager
+import es.timasostima.robank.home.HomeScreen
+import es.timasostima.robank.ui.theme.RobankTheme
+
+@Composable
+fun App(
+    changeMode: (Boolean?) -> Unit,
+    db: Database,
+    accountManager: AccountManager
+) {
+    val context = LocalContext.current
+
+    val categoriesList: MutableList<CategoryData> = mutableListOf()
+    val billsList: MutableList<BillData> = mutableListOf()
+    val goalsList: MutableList<GoalData> = mutableListOf()
+    val preferences = PreferencesData()
+    db.loadCategories(categoriesList)
+    db.loadBills(billsList)
+    db.loadGoals(goalsList)
+    db.loadPreferences(preferences, changeMode, context)
+
+    val mainController = rememberNavController()
+    val navBackStack by mainController.currentBackStackEntryAsState()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { MainNavigationBar(mainController, navBackStack) }
+    ) { innerPadding ->
+        NavHost(
+            navController = mainController,
+            startDestination = "home",
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("home") {
+                HomeScreen(categoriesList, goalsList, db)
+            }
+            composable("config") {
+                ConfigScreen(preferences, db, accountManager)
+            }
+            composable("charts") {
+                Charts(billsList, categoriesList)
+            }
+        }
+    }
+}
+
+@Composable
+fun MainNavigationBar(
+    mainController: NavHostController,
+    navBackStack: NavBackStackEntry?
+) {
+    NavigationBar(
+        tonalElevation = 25.dp,
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = Modifier.topBorder(1.dp, MaterialTheme.colorScheme.onSecondaryContainer)
+    ) {
+        items.forEach { item ->
+            val isSelected = item.title.lowercase() == navBackStack?.destination?.route
+            NavigationBarItem(
+                selected = isSelected,
+                icon = {
+                    val tint by animateColorAsState(
+                        if (isSelected)
+                            MaterialTheme.colorScheme.primary
+                        else
+                            MaterialTheme.colorScheme.onSecondaryContainer, label = ""
+                    )
+
+                    Icon(
+                        imageVector = ImageVector.vectorResource(item.icon),
+                        contentDescription = item.title,
+                        tint = tint
+                    )
+                },
+                onClick = {
+                    mainController.navigate(item.title.lowercase()) {
+                        popUpTo(mainController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
+        }
+    }
+}
+
+data class BottomNavigation(
+    val title: String,
+    val icon: Int
+)
+
+val items = listOf(
+    BottomNavigation("Config", R.drawable.config),
+    BottomNavigation("Home", R.drawable.home),
+    BottomNavigation("Charts", R.drawable.charts)
+)
+
+fun Modifier.topBorder(strokeWidth: Dp, color: Color) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = 0f),
+                end = Offset(x = width, y = 0f),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
+
+@Preview
+@Composable
+fun ButtomBar() {
+    RobankTheme {
+        MainNavigationBar(rememberNavController(), null)
+    }
+}
--- app/src/main/java/es/timasostima/robank/MainActivity.kt
+++ app/src/main/java/es/timasostima/robank/MainActivity.kt
@@ -5,37 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.firebase.FirebaseApp.initializeApp
+import es.timasostima.robank.enterApp.EnterApp
 import es.timasostima.robank.ui.theme.RobankTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,8 +24,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RobankTheme {
-                var showSystemUi by remember { mutableStateOf(false) }
+            initializeApp(this)
+            var darkmode: Boolean? by rememberSaveable { mutableStateOf(null) }
+            val changeMode: (Boolean?) -> Unit = {
+                darkmode = it
+            }
+            RobankTheme(darkTheme = darkmode ?: isSystemInDarkTheme()) {
+                val showSystemUi by remember { mutableStateOf(false) }
                 val view = LocalView.current
                 val window = (view.context as Activity).window
                 val insetsController = WindowCompat.getInsetsController(window, view)
@@ -52,79 +38,42 @@ class MainActivity : ComponentActivity() {
                     if (!showSystemUi) {
                         insetsController.apply {
                             hide(WindowInsetsCompat.Type.systemBars())
-                            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                            systemBarsBehavior =
+                                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                         }
-                    } else { insetsController.apply { show(WindowInsetsCompat.Type.systemBars()) } }
+                    } else {
+                        insetsController.apply { show(WindowInsetsCompat.Type.systemBars()) }
+                    }
                 }
-
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    bottomBar = { MyBar() }) { innerPadding ->
-                    App(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                App(changeMode)
+                EnterApp(changeMode)
             }
+
+//            MainScreen(this)
         }
     }
-}
 
-@Composable
-fun MyBar(){
-    Row (modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly){
-        Button(onClick = {}) { }
-        Button(onClick = {}) { }
-        Button(onClick = {}) { }
+    override fun onStart() {
+//        println("lalalalala")
+        super.onStart()
+    }
 
+    override fun onResume() {
+//        println("jajajaja")
+        super.onResume()
+    }
+
+    override fun onPause() {
+//        println("sasasas")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
 
-@Composable
-fun App(modifier: Modifier = Modifier){
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier.fillMaxSize().background(Color(41,43,63))
-    ){
-        Image(
-            painter = painterResource(R.drawable.logo),
-            contentDescription = "",
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(0.4f)
-        )
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth(0.8f).fillMaxHeight(0.6f)
-        ){
-            Spacer(modifier = Modifier.size(20.dp))
-            TextField(
-                "",
-                onValueChange = {},
-                placeholder = { Text("name") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            TextField(
-                "",
-                onValueChange = {},
-                placeholder = { Text("password") },
-                modifier = Modifier.fillMaxWidth(),
-            )
-            Spacer(modifier = Modifier.size(20.dp))
-
-            Button(onClick = {}, modifier = Modifier.fillMaxWidth(0.5f)) {
-                Text("Log In")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RobankTheme {
-        App()
-    }
-}
