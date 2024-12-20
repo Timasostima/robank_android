4983b2167c43c1bc421ea540a8888e261005be36
Tymur <tymurkulivar@gmail.com>
2024-12-20 22:05:12 +0100

    update RobankTextField

--- app/src/main/java/es/timasostima/robank/enterApp/EnterApp.kt
+++ app/src/main/java/es/timasostima/robank/enterApp/EnterApp.kt
@@ -3,14 +3,12 @@ package es.timasostima.robank.enterApp
 import androidx.activity.ComponentActivity
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,12 +25,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import es.timasostima.robank.App
+import es.timasostima.robank.R
 import es.timasostima.robank.database.Database
 
 @Composable
@@ -93,6 +94,8 @@ fun RobankTextField(
     transformation: VisualTransformation = VisualTransformation.None,
     trailingIcon: @Composable () -> Unit = {},
 ){
+    val options = if (placeholder == stringResource(R.string.email)) KeyboardOptions(keyboardType = KeyboardType.Email)
+    else KeyboardOptions.Default
     TextField(
         text,
         onValueChange = updateText,
@@ -100,47 +103,13 @@ fun RobankTextField(
         singleLine = true,
         isError = !isValid,
         colors = tfC,
+        keyboardOptions = options,
         visualTransformation = transformation,
         modifier = Modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(20.dp)),
         trailingIcon = {
             trailingIcon()
-        }
-    )
-}
-
-@Composable
-fun RobankDialog( ///////////////////////////////////////////////////////////////////////////////////
-    title: String,
-    onClose: () -> Unit,
-    errors: Map<String, Boolean>? = null
-) {
-    AlertDialog(
-        onDismissRequest = onClose,
-        title = { Text(title) },
-        text = {
-            if (errors == null){
-                Text("Check your email")
-            }
-            else{
-                Column {
-                    for (error in errors) {
-                        val color = if (error.value) MaterialTheme.colorScheme.error
-                        else MaterialTheme.colorScheme.primary
-
-                        Text(error.key, color = color)
-                    }
-                }
-            }
-
-        },
-        confirmButton = {
-            Button(
-                onClick = onClose
-            ) {
-                Text("OK")
-            }
         }
     )
 }
